0945b9ffcad101cd62c6057cd3d57ed37ceb3afd
neelofarakhtar <neelofaryuvasoft387@gmail.com>
2022-09-09 18:39:10 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :questions
+  has_many :answers
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
